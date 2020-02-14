7b6d9f1dd0edac803fc94c1c81b8a698d38da7db
Rob Terrell <Rterrell25@gmail.com>
2020-02-14 14:13:07 -0500

    added form validation for email, and passwords

--- functions/index.js
+++ functions/index.js
@@ -63,12 +63,36 @@ app.post("/job", (req, res) => {
     })
 })
 
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (email.match(regEx)) return true
+  else return false
+}
+
+const isEmpty = string => {
+  if (string.trim() === "") return true
+  else return false
+}
+
 app.post("/signup", (req, res) => {
   const newUser = {
     email: req.body.email,
     password: req.body.password,
     confirmPassword: req.body.confirmPassword
   }
+
+  let errors = {}
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty"
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email"
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty"
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = "Passwords must match"
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors)
 
   let token, userId
   firebase
