8069b7680a0d9977c137ac1ef8a51fc77728e523
Rob Terrell <Rterrell25@gmail.com>
2020-02-16 13:34:57 -0500

    set up user details route for individuals to update and edit their bio

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -100,6 +100,16 @@ exports.addUserDetails = (req, res) => {
 
 exports.addUserDetails = (req, res) => {
   let userDetails = reduceUserDetails(req.body)
+
+  db.doc(`/users/${req.user.uid}`)
+    .update(userDetails)
+    .then(() => {
+      return res.json({ message: "Details added successfully" })
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
 }
 
 exports.uploadImage = (req, res) => {
