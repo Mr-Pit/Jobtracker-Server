d1b5d22ffb2b1ab6c1f3d90ed04666c700f16e0c
Rob Terrell <Rterrell25@gmail.com>
2020-02-15 22:04:41 -0500

    added an image upload route

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const app = require("express")()
 const FBAuth = require("./utility/fbAuth")
 
 const { getAllJobs, postOneJob } = require("./handlers/jobs")
-const { signup, login } = require("./handlers/users")
+const { signup, login, uploadImage } = require("./handlers/users")
 
 // Initialize Firebase
 
@@ -15,5 +15,6 @@ app.post("/job", FBAuth, postOneJob)
 // users route
 app.post("/signup", signup)
 app.post("/login", login)
+app.post("/user/image", FBAuth, uploadImage)
 
 exports.api = functions.https.onRequest(app)
