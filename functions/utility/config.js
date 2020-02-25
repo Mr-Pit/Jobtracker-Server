3e38b78abe957f0cd9cc074b28a173093c23dc00
Rob Terrell <Rterrell25@gmail.com>
2020-02-25 15:44:27 -0500

    set up update job route

--- functions/utility/config.js
+++ functions/utility/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: 'AIzaSyBk91aN4lXYbbfx5YRzkSoHbsqCplBcj0A',
-  authDomain: 'jobtracker-4f14f.firebaseapp.com',
-  databaseURL: 'https://jobtracker-4f14f.firebaseio.com',
-  projectId: 'jobtracker-4f14f',
-  storageBucket: 'jobtracker-4f14f.appspot.com',
-  messagingSenderId: '175605853072',
-  appId: '1:175605853072:web:4121365141a81ba6c46115',
-  measurementId: 'G-E0XJ9YB86S'
+  apiKey: "AIzaSyBk91aN4lXYbbfx5YRzkSoHbsqCplBcj0A",
+  authDomain: "jobtracker-4f14f.firebaseapp.com",
+  databaseURL: "https://jobtracker-4f14f.firebaseio.com",
+  projectId: "jobtracker-4f14f",
+  storageBucket: "jobtracker-4f14f.appspot.com",
+  messagingSenderId: "175605853072",
+  appId: "1:175605853072:web:4121365141a81ba6c46115",
+  measurementId: "G-E0XJ9YB86S"
 }
